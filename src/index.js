942e0a2d10fa1a968c348157f6a8c69c0d544d26
meppsilon <mepps32@gmail.com>
2018-03-26 09:52:02 -0700

    Add latest code

--- src/index.js
+++ src/index.js
@@ -1,9 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { applyMiddleware, createStore, combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
+import { createLogger } from 'redux-logger';
+import App from 'components/App';
 import 'css/index.css';
 import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootReducer = combineReducers({
+  form: formReducer,
+});
+
+const logger = createLogger({
+  collapsed: true,
+});
+
+const store = createStore(
+  rootReducer,
+  applyMiddleware(logger),
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
+);
 registerServiceWorker();
