00cd3dcb6619a7cb4d5cc812e4208ac80b91ad7a
meppsilon <mepps32@gmail.com>
2018-03-27 13:36:33 -0700

    Add mode button container

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,14 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { fetchHierarchy, toggleMode } from 'actions/hierarchy';
+import { fetchHierarchy } from 'actions/hierarchy';
 import { connect } from 'react-redux';
-import Immutable from 'immutable';
 import Hierarchy from './Hierarchy';
 import ComponentDrawer from './ComponentDrawer';
 import ModeButton from './ModeButton';
 
 class App extends Component {
   static propTypes = {
-    hierarchy: PropTypes.instanceOf(Immutable.Map).isRequired,
     fetchHierarchy: PropTypes.func.isRequired,
-    toggleMode: PropTypes.func.isRequired,
   };
 
   componentWillMount() {
@@ -19,14 +16,10 @@ class App extends Component {
   }
 
   render() {
-    const { hierarchy } = this.props;
     return (
       <div className="App">
         <div className="mt-6 ml-8">
-          <ModeButton
-            mode={hierarchy.get('mode')}
-            onClick={this.props.toggleMode}
-          />
+          <ModeButton />
         </div>
         <Hierarchy />
         <ComponentDrawer />
@@ -35,7 +28,6 @@ class App extends Component {
   }
 }
 
-export default connect(state => ({ hierarchy: state.hierarchy }), {
+export default connect(null, {
   fetchHierarchy,
-  toggleMode,
 })(App);
